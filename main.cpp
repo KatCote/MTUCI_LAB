1bdf5a846de91997ab798ee36faa6ebfb8cf26fd
KatCote <filtermr@gmail.com>
2023-09-06 06:20:07 -0500

    06.09.2023 #2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <iostream>
 
-int main (void)
+int main ()
 {
-	printf("Laba Laba Laba Laba Laba\n");
+	int A;
+	float B;
+	float SUM;
+
+	std::cout << "Input 2 nums"
+		<< "\nFirst must be integer:\t";
+	std::cin >> A;
+	std::cout << "\nSecond can be float:\t";
+	std::cin >> B;
+	
+	SUM = A + B;
+
+	std::cout << "\nSum equals " << SUM << "\n";
+
 	return 0;
 }
