dead98a81bcff22d64a2c77e280f3542cba355e3
KatCote <filtermr@gmail.com>
2023-09-08 14:37:18 -0500

    Lab. #1 - Commit #7

--- compile.sh
+++ compile.sh
@@ -1,3 +1,18 @@
 ### bash
-toilet Laba --gay
-clang++ -o laba.out main.cpp
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo "\n"
+toilet Laboratory Work --gay -f smbraille
+echo "\n"
+
+if clang++ -o laba.out main.cpp;
+then
+	echo "${GREEN}Compile Successful${NC}\n"
+	chmod +x laba.out
+	./laba.out
+else
+	echo "\n${RED}Compile Failed${NC}\n"
+fi
